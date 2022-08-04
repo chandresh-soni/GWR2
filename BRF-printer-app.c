7775d0d73be21c0e6a5ce7f0136f6c2e6edab394
chandresh2702 <chandresh2702@gmail.com>
2022-08-04 16:33:52 +0530

    coding start

---
+++ BRF-printer-app.c
@@ -0,0 +1,8 @@
+# include <pappl/pappl.h>
+# include <ppd/ppd.h>
+# include <cupsfilters/log.h>
+# include <cupsfilters/filter.h>
+# include <cups/cups.h>
+# include <string.h>
+# include <limits.h>
+
