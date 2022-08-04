bfbd97b1c0262a313ad1902d13af9b0b610b4a23
chandresh2702 <chandresh2702@gmail.com>
2022-08-04 19:57:14 +0530

    update 3 aug

--- BRF-printer-app.c
+++ BRF-printer-app.c
@@ -1,9 +1,23 @@
 # include <pappl/pappl.h>
-# include <ppd.h>
-# include <log.h>
-# include <filter.h>
+# include <ppd/ppd.h>
+# include <cupsfilters/log.h>
+# include <cupsfilters/filter.h>
 # include <cups/cups.h>
 # include <string.h>
 # include <limits.h>
 
 
+
+
+
+//
+// 'main()' - Main entry for the brf-printer-app.
+//
+
+int
+main(int  argc,				// I - Number of command-line arguments
+     char *argv[])			// I - Command-line arguments
+{
+  papplMainloop(argc, argv, "1.0", NULL, 0, NULL, NULL, NULL, NULL, NULL, system_cb, NULL, NULL);
+  return (0);
+}
