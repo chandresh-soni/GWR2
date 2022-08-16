f8aad40fb0fd8f79e9138aae77d14d72184d1863
chandresh2702 <chandresh2702@gmail.com>
2022-08-16 11:29:35 +0530

    changes

--- generic-brf.c
+++ generic-brf.c
@@ -1,11 +1,4 @@
-//
-// EPL2 driver for LPrint, a Label Printer Application
-//
-// Copyright © 2019-2021 by Michael R Sweet.
-//
-// Licensed under Apache License v2.0.  See the file "LICENSE" for more
-// information.
-//
+
 
 //
 // Include necessary headers...
